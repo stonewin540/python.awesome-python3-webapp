16c3ccf4de100af62c5020ba72f76495ce580fdf
stonewin540 <stonewin540@163.com>
2017-06-30 22:46:27 +0800

    Insert users;

--- www/handlers.py
+++ www/handlers.py
@@ -9,6 +9,30 @@ from www.models import User, Comment, Blog, next_id
 @get('/')
 async def index(request):
     users = await User.findAll()
+
+    # # test start
+    # words = {
+    #     0: 'a',
+    #     1: 'b',
+    #     2: 'c',
+    #     3: 'd',
+    #     4: 'e',
+    #     5: 'f',
+    #     6: 'g',
+    #     7: 'h',
+    #     8: 'i',
+    #     9: 'j',
+    #     10: 'k',
+    # }
+    # max_count = 10
+    # count = len(users)
+    # if count < max_count:
+    #     for i in range(count, max_count + 1):
+    #         u = User(name='test%d' % i, email='%d@%s.com' % (i, words[i]), passwd='p%s' % i, image='about: blank')
+    #         await u.save()
+    #         users.append(u)
+    # # test end
+
     return {
         '__template__': 'test.html',
         'users': users
