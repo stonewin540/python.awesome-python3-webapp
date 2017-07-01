1e2c4d70f85908e948edb0a9be7d2b570d82c949
stonewin540 <stonewin540@163.com>
2017-07-01 21:43:28 +0800

    Oh my gosh, it works, displayed the blog data on browser!!!

--- www/handlers.py
+++ www/handlers.py
@@ -6,34 +6,50 @@ from www.coroweb import get, post
 from www.models import User, Comment, Blog, next_id
 
 
+# @get('/')
+# async def index(request):
+#     users = await User.findAll()
+#
+#     # # test start
+#     # words = {
+#     #     0: 'a',
+#     #     1: 'b',
+#     #     2: 'c',
+#     #     3: 'd',
+#     #     4: 'e',
+#     #     5: 'f',
+#     #     6: 'g',
+#     #     7: 'h',
+#     #     8: 'i',
+#     #     9: 'j',
+#     #     10: 'k',
+#     # }
+#     # max_count = 10
+#     # count = len(users)
+#     # if count < max_count:
+#     #     for i in range(count, max_count + 1):
+#     #         u = User(name='test%d' % i, email='%d@%s.com' % (i, words[i]), passwd='p%s' % i, image='about: blank')
+#     #         await u.save()
+#     #         users.append(u)
+#     # # test end
+#
+#     return {
+#         '__template__': 'test.html',
+#         'users': users
+#     }
+
 @get('/')
-async def index(request):
-    users = await User.findAll()
-
-    # # test start
-    # words = {
-    #     0: 'a',
-    #     1: 'b',
-    #     2: 'c',
-    #     3: 'd',
-    #     4: 'e',
-    #     5: 'f',
-    #     6: 'g',
-    #     7: 'h',
-    #     8: 'i',
-    #     9: 'j',
-    #     10: 'k',
-    # }
-    # max_count = 10
-    # count = len(users)
-    # if count < max_count:
-    #     for i in range(count, max_count + 1):
-    #         u = User(name='test%d' % i, email='%d@%s.com' % (i, words[i]), passwd='p%s' % i, image='about: blank')
-    #         await u.save()
-    #         users.append(u)
-    # # test end
-
+def index(request):
+    summary = \
+        'Lorem ipsum dolor sit amet, ' \
+        'consectetur adipisicing elit, ' \
+        'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200),
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
